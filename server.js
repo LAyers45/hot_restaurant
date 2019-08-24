7bcc92c11b69fecf28ece7414c03b72508d1b5f0
Sunny <loganayers2013@yahoo.com>
2019-08-24 10:52:37 -0500

    updated java files

--- server.js
+++ server.js
@@ -7,3 +7,51 @@ var path = require("path");
 // =============================================================
 var app = express();
 var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Reservations (DATA)
+// =============================================================
+var tables = [];
+
+var reservations = [];
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/reserve", function (req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+// Displays all characters
+app.get("/tables", function (req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+// Create reservations - takes in JSON input
+if (tables.length) {
+    app.post("/tables", function (req, res) {
+        // req.body hosts is equal to the JSON post sent from the user
+        // This works because of our body parsing middleware
+        var tables = req.body;
+
+
+        console.log(tables);
+
+        characters.push(tables);
+
+        res.json(tables);
+    });
+}
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
